026c49f55079af4a073ca3594802332c4e0ef5e2
avalynn <avalynndev@gmail.com>
2025-09-28 16:06:23 +0530

    fixed vision

--- Scripts/GlobalManager.gd
+++ Scripts/GlobalManager.gd
@@ -14,15 +14,19 @@ var knowledge_sacrificed : bool = false
 var player_dead : bool = false
 var vision_sac := false
 
-signal  player_won()
+signal player_won()
 signal lose_knowledge()
 signal lose_health()
-signal  lose_speed()
+signal lose_speed()
 
 func _ready() -> void:
 	current_lev = level_number
 	change_level = false
-	
+	# Hide vision panel by default
+	if visionpanel:
+		visionpanel.visible = false
+	else:
+		push_error("Vision panel not found!")
 
 func _process(_delta: float) -> void:
 	if not level_cleared:
@@ -30,36 +34,33 @@ func _process(_delta: float) -> void:
 		if enemies.is_empty() and not change_level:
 			level_cleared = true 
 			change_level_after_delay()
-		if vision_sac:
-			visionpanel.visible = true
 
 func change_level_after_delay() -> void:
 	change_level = true
-	await get_tree().create_timer(.0).timeout
+	await get_tree().create_timer(0.0).timeout
 	get_tree().paused = false
 	current_lev += 1
 	if leveltochange:
 		emit_signal("player_won")
 		DialogueManager.show_example_dialogue_balloon(sacrificedialogue, "start")
 
-
 func sac_knowledge():
 	emit_signal("lose_knowledge")
 
-func  sac_health():
+func sac_health():
 	emit_signal("lose_health")
 
-func  sac_speed():
+func sac_speed():
 	emit_signal("lose_speed")
-	
+
 func sac_vision():
 	vision_sac = true
-	visionpanel.visible = true
-	
-	
+	if visionpanel:
+		visionpanel.get_parent().remove_child(visionpanel)
+		get_tree().root.add_child(visionpanel)
+		visionpanel.top_level = true
+		visionpanel.visible = true
+		print("Vision panel is now permanent")
+
 func _finished():
 	get_tree().change_scene_to_packed(leveltochange)
-
-
-
-	
